5554da25089fcb6fffb38011ea095b64a47c6959
[ryan larson] <[rylarso@gmail.com]>
2022-06-27 11:22:28 -0600

    pages working and api data displayed

--- app.js
+++ app.js
@@ -3,12 +3,29 @@ const API_URL = "https://fakestoreapi.com";
 var app = new Vue({
     el: "#app",
     data: {
-        apiData: ""
+        currentPage: "Welcome",
+        apiData: "",
+        checkoutItems: [],
     },
     created: async function () {
         let response = await fetch(API_URL + "/products");
         let data = await response.json();
         console.log(data);
         this.apiData = data;
+    },
+    methods: {
+        gotoStore: function () {
+            this.currentPage = "Store";
+            console.log(this.currentPage)
+        },
+
+        gotoCheckout: function () {
+            this.currentPage = "Checkout";
+            console.log(this.currentPage);
+        },
+        gotoHome: function () {
+            this.currentPage = "Welcome";
+            console.log(this.currentPage);
+        },
     }
 });
